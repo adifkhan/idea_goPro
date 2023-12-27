a19c395e5c8857d90fa96ca2cf2159e611ca471e
adifkhan <pkadif@gmail.com>
2023-12-27 15:27:31 +0600

    responsiveness

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 /* ========== toggle icon navbar ==============  */
 let menuIcon = document.querySelector("#menu_dropdown");
 let navbar = document.querySelector(".mini_navbar");
+let searchIcon = document.querySelector(".search_icon");
+let searchBox = document.querySelector(".search_box");
 
 menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active_navbar");
-  console.log(menuIcon, navbar);
+};
+searchIcon.onclick = () => {
+  searchBox.classList.toggle("active_search");
 };
